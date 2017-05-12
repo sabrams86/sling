8f37b88bee6eadd9895d39d52a5dd63bcf4c1538
Steven Abrams <steven.abrams86@gmail.com>
2017-05-12 14:33:44 -0600

    add jwt auth check on page refresh

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,12 +1,28 @@
 // @flow
 import React, { Component } from 'react';
 import { BrowserRouter, Match, Miss } from 'react-router';
+import { connect } from 'react-redux';
+import { authenticate } from '../../actions/session';
 import Home from '../Home';
 import NotFound from '../../components/NotFound';
 import Login from '../Login';
 import Signup from '../Signup';
 
+type Props = {
+  authenticate: () => void,
+}
+
 class App extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.props.authenticate();
+    }
+  }
+
+  props: Props
+
   render() {
     return (
       <BrowserRouter>
@@ -21,4 +37,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { authenticate }
+)(App);
